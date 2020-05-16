95cc632020fcd51de520fda1886bc24773b3fdfe
Mopheta <kevinstalker14@gmail.com>
2020-05-16 11:06:06 -0300

    Updates

---
+++ precache-manifest.7be04723ad95a99c7fb5bee2e40e807b.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35c8a0e6da3c7d8a592df73e02eb2261",
+    "url": "/Mopheta/Mopheta.github.io.git/index.html"
+  },
+  {
+    "revision": "180bdd80fcb71aa684da",
+    "url": "/Mopheta/Mopheta.github.io.git/static/css/main.09769023.chunk.css"
+  },
+  {
+    "revision": "8714b013b6ff343559c1",
+    "url": "/Mopheta/Mopheta.github.io.git/static/js/2.41c5c4a6.chunk.js"
+  },
+  {
+    "revision": "7694ed5eff56d74dfc4b6ac3fc78a12b",
+    "url": "/Mopheta/Mopheta.github.io.git/static/js/2.41c5c4a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "180bdd80fcb71aa684da",
+    "url": "/Mopheta/Mopheta.github.io.git/static/js/main.2b3826fa.chunk.js"
+  },
+  {
+    "revision": "f17098fea45c339a37b9",
+    "url": "/Mopheta/Mopheta.github.io.git/static/js/runtime-main.b288dc23.js"
+  },
+  {
+    "revision": "7a7d99e5f10f449e7f4a48b8f94fdf94",
+    "url": "/Mopheta/Mopheta.github.io.git/static/media/code-banner.7a7d99e5.jpg"
+  },
+  {
+    "revision": "ca70257e0634582d4e07ea0edff6c10f",
+    "url": "/Mopheta/Mopheta.github.io.git/static/media/computerdevImage.ca70257e.jpg"
+  },
+  {
+    "revision": "12d80e494c79e46f295a97e2bfc8c0bd",
+    "url": "/Mopheta/Mopheta.github.io.git/static/media/education.12d80e49.jpg"
+  },
+  {
+    "revision": "01cd79d8e081990962f24f759f733c4b",
+    "url": "/Mopheta/Mopheta.github.io.git/static/media/experience.01cd79d8.jpg"
+  },
+  {
+    "revision": "ce806778bea6c84ccf86328d9fac6ae8",
+    "url": "/Mopheta/Mopheta.github.io.git/static/media/fcea.ce806778.jpg"
+  },
+  {
+    "revision": "177764eadf43efc393ebb98329a774b2",
+    "url": "/Mopheta/Mopheta.github.io.git/static/media/linea-basic-10.177764ea.svg"
+  },
+  {
+    "revision": "61f3a8a387a64abfc0535cd0813cd71a",
+    "url": "/Mopheta/Mopheta.github.io.git/static/media/linea-basic-10.61f3a8a3.woff"
+  },
+  {
+    "revision": "e4de528c00da0c081906c3caea289f0f",
+    "url": "/Mopheta/Mopheta.github.io.git/static/media/linea-basic-10.e4de528c.eot"
+  },
+  {
+    "revision": "e6763f008205c55db623cdf68bbd581f",
+    "url": "/Mopheta/Mopheta.github.io.git/static/media/linea-basic-10.e6763f00.ttf"
+  },
+  {
+    "revision": "1dde66c9b128b768c9596c748a89360c",
+    "url": "/Mopheta/Mopheta.github.io.git/static/media/ortlogo.1dde66c9.jpg"
+  },
+  {
+    "revision": "0e16f330530be7cf229c861916abff07",
+    "url": "/Mopheta/Mopheta.github.io.git/static/media/profile.0e16f330.jpg"
+  },
+  {
+    "revision": "bc11fa19de658e736a022675c8f0dcf2",
+    "url": "/Mopheta/Mopheta.github.io.git/static/media/unit.bc11fa19.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Mopheta/Mopheta.github.io.git/precache-manifest.7be04723ad95a99c7fb5bee2e40e807b.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Mopheta/Mopheta.github.io.git/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
